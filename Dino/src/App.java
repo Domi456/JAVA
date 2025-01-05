3fa957a36870d652dd902e24a205500bc8ff78d0
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-01-05 16:57:56 +0100

    added 'Dino' project

---
+++ Dino/src/App.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("Dino game");
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dino dino = new Dino();
+        frame.add(dino);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ Dino/src/Dino.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Dino extends JPanel{
+    
+}
