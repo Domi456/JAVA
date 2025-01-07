96a94f7666529686d58e9d4887b2f091e5300815
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-01-07 07:54:21 +0100

    game works

--- Dino/src/Dino.java
+++ Dino/src/Dino.java
@@ -36,9 +36,23 @@ public class Dino extends JPanel implements ActionListener, KeyListener{
 
     Block dinosaur;
     Timer gameLoop;
+    Timer placeCactusTimer;
+    boolean gameOver = false;
+    int score = 0;
+
+    //cactus
+    int cactus1Width = 34;
+    int cactus2Width = 69;
+    int cactus3Width = 102;
+    int cactusHeight = 70;
+    int cactusX = 700;
+    int cactusY = boardHeight - cactusHeight;
+    ArrayList<Block> cactusArrayList;
 
     // game physics
+    int veloX = -12;
     int veloY = 0;
+    int gravity = 1;
 
     public Dino() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -54,9 +68,41 @@ public class Dino extends JPanel implements ActionListener, KeyListener{
         cactus3Image = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
 
         dinosaur = new Block(dinoX, dinoY, dinoWidth, dinoHeight, dinoImage);
+        cactusArrayList = new ArrayList<Block>();
 
         gameLoop = new Timer(1000/60, this);
         gameLoop.start();
+
+        placeCactusTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                placeCactus();
+            }
+        });
+        placeCactusTimer.start();
+    }
+
+    public void placeCactus(){
+        if (gameOver) {
+            return;
+        }
+        double placeCactusChance = Math.random();
+        if(placeCactusChance > .90){    // 10% chance
+            Block cactus = new Block(cactusX, cactusY, cactus3Width, cactusHeight, cactus3Image);
+            cactusArrayList.add(cactus);
+        }
+        else if(placeCactusChance > .70){   // 20% chance
+            Block cactus = new Block(cactusX, cactusY, cactus2Width, cactusHeight, cactus2Image);
+            cactusArrayList.add(cactus);
+        }
+        else if(placeCactusChance > .50){   // 20% chance
+            Block cactus = new Block(cactusX, cactusY, cactus1Width, cactusHeight, cactus1Image);
+            cactusArrayList.add(cactus);
+        }
+
+        if(cactusArrayList.size() > 10){
+            cactusArrayList.remove(0);  // remove the 1st cactus
+        }
     }
 
     public void paintComponent(Graphics g){
@@ -66,21 +112,81 @@ public class Dino extends JPanel implements ActionListener, KeyListener{
 
     public void draw(Graphics g){        
         g.drawImage(dinosaur.img, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+        for (int i = 0; i < cactusArrayList.size(); i++) {
+            Block cactus = cactusArrayList.get(i);
+            g.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height, null);
+        }
+        g.setColor(Color.black);
+        g.setFont(new Font("Courier", Font.PLAIN, 24));
+        if(gameOver){
+            g.drawString("GAME OVER " + String.valueOf(score), 10, 35);
+        }
+        else{
+            g.drawString("SCORE " + String.valueOf(score), 10, 35);
+        }
+    }
+
+    public void move(){
+        veloY += gravity;
+        dinosaur.y += veloY;
+
+        if(dinosaur.y > dinoY){
+            dinosaur.y = dinoY;
+            veloY = 0;
+            dinosaur.img = dinoImage;
+        }
+        for (int i = 0; i < cactusArrayList.size(); i++) {
+            Block cactus = cactusArrayList.get(i);
+            cactus.x += veloX;
+            if(collision(dinosaur, cactus)){
+                gameOver = true;
+                dinosaur.img = deadDinoImage;
+            }
+        }
+        score++;
+    }
+
+    public boolean collision(Block a, Block b){
+        return a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        move();
         repaint();
+        if(gameOver){
+            gameLoop.stop();
+            placeCactusTimer.stop();
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_O){
-            System.out.println("JUMP");
+            //System.out.println("JUMP");
+            if(dinosaur.y == dinoY){
+                veloY = -17;
+                dinosaur.img = dinoJumpImage;
+            }
+        }
+        if(gameOver){
+            // restart game
+            dinosaur.y = dinoY;
+            dinosaur.img = dinoImage;
+            veloY = 0;
+            cactusArrayList.clear();
+            score = 0;
+            gameOver = false;
+            gameLoop.start();
+            placeCactusTimer.start();
         }
     }
 
     // not used, but necessary
+    //
     @Override
     public void keyReleased(KeyEvent e) {}
     @Override
