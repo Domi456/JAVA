4353af6cd972dcdeb69dde6549aa2465c7c559f0
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-01-20 09:43:38 +0100

    for some reason it doesnt play the sound

---
+++ MorseCode/src/MorseBackend.java
@@ -0,0 +1,180 @@
+import java.util.HashMap;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class MorseBackend {
+    // hashmap = key/value párok
+    private HashMap<Character, String> morseCodeMap;
+    private SourceDataLine sourceDataLine;
+
+    public MorseBackend() {
+        morseCodeMap = new HashMap<>();
+        
+        // uppercase
+        morseCodeMap.put('A', ".-");
+        morseCodeMap.put('B', "-...");
+        morseCodeMap.put('C', "-.-.");
+        morseCodeMap.put('D', "-..");
+        morseCodeMap.put('E', ".");
+        morseCodeMap.put('F', "..-.");
+        morseCodeMap.put('G', "--.");
+        morseCodeMap.put('H', "....");
+        morseCodeMap.put('I', "..");
+        morseCodeMap.put('J', ".---");
+        morseCodeMap.put('K', "-.-");
+        morseCodeMap.put('L', ".-..");
+        morseCodeMap.put('M', "--");
+        morseCodeMap.put('N', "-.");
+        morseCodeMap.put('O', "---");
+        morseCodeMap.put('P', ".--.");
+        morseCodeMap.put('Q', "--.-");
+        morseCodeMap.put('R', ".-.");
+        morseCodeMap.put('S', "...");
+        morseCodeMap.put('T', "-");
+        morseCodeMap.put('U', "..-");
+        morseCodeMap.put('V', "...-");
+        morseCodeMap.put('W', ".--");
+        morseCodeMap.put('X', "-..-");
+        morseCodeMap.put('Y', "-.--");
+        morseCodeMap.put('Z', "--..");
+
+        // lowercase
+        morseCodeMap.put('a', ".-");
+        morseCodeMap.put('b', "-...");
+        morseCodeMap.put('c', "-.-.");
+        morseCodeMap.put('d', "-..");
+        morseCodeMap.put('e', ".");
+        morseCodeMap.put('f', "..-.");
+        morseCodeMap.put('g', "--.");
+        morseCodeMap.put('h', "....");
+        morseCodeMap.put('i', "..");
+        morseCodeMap.put('j', ".---");
+        morseCodeMap.put('k', "-.-");
+        morseCodeMap.put('l', ".-..");
+        morseCodeMap.put('m', "--");
+        morseCodeMap.put('n', "-.");
+        morseCodeMap.put('o', "---");
+        morseCodeMap.put('p', ".--.");
+        morseCodeMap.put('q', "--.-");
+        morseCodeMap.put('r', ".-.");
+        morseCodeMap.put('s', "...");
+        morseCodeMap.put('t', "-");
+        morseCodeMap.put('u', "..-");
+        morseCodeMap.put('v', "...-");
+        morseCodeMap.put('w', ".--");
+        morseCodeMap.put('x', "-..-");
+        morseCodeMap.put('y', "-.--");
+        morseCodeMap.put('z', "--..");
+
+        // numbers
+        morseCodeMap.put('0', "-----");
+        morseCodeMap.put('1', ".----");
+        morseCodeMap.put('2', "..---");
+        morseCodeMap.put('3', "...--");
+        morseCodeMap.put('4', "....-");
+        morseCodeMap.put('5', ".....");
+        morseCodeMap.put('6', "-....");
+        morseCodeMap.put('7', "--...");
+        morseCodeMap.put('8', "---..");
+        morseCodeMap.put('9', "----.");
+
+        // special characters
+        morseCodeMap.put(' ', "/");
+        morseCodeMap.put(',', "--..--");
+        morseCodeMap.put('.', ".-.-.-");
+        morseCodeMap.put('?', "..--..");
+        morseCodeMap.put(';', "-.-.-.");
+        morseCodeMap.put(':', "---...");
+        morseCodeMap.put('(', "-.--.");
+        morseCodeMap.put(')', "-.--.-");
+        morseCodeMap.put('[', "-.--.");
+        morseCodeMap.put(']', "-.--.-");
+        morseCodeMap.put('{', "-.--.");
+        morseCodeMap.put('}', "-.--.-");
+        morseCodeMap.put('+', ".-.-.");
+        morseCodeMap.put('-', "-....-");
+        morseCodeMap.put('_', "..--.-");
+        morseCodeMap.put('"', ".-..-.");
+        morseCodeMap.put('\'', ".----.");
+        morseCodeMap.put('/', "-..-.");
+        morseCodeMap.put('\\', "-..-.");
+        morseCodeMap.put('@', ".--.-.");
+        morseCodeMap.put('=', "-...-");
+        morseCodeMap.put('!', "-.-.--");
+    }
+
+    public String translateToMorse(String textToTranslate){
+        StringBuilder translated = new StringBuilder();
+        for (Character letter : textToTranslate.toCharArray()) {
+            translated.append(morseCodeMap.get(letter) + " ");
+        }
+        return translated.toString();
+    }
+
+    public void playSound(String[] morseMessage){
+        AudioFormat audioFormat = new AudioFormat(44100, 16, 1, true, false);
+        DataLine.Info datInfo = new DataLine.Info(SourceDataLine.class, audioFormat);
+        try{
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(datInfo);
+            sourceDataLine.open(audioFormat);
+            sourceDataLine.start();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }        
+
+        int dotDuration = 200;
+        int dashDuration = (int) (1.5 * dotDuration);
+        int slashDuration = 2 * dashDuration;
+
+        for (String pattern : morseMessage) {
+            for (char c : pattern.toCharArray()) {
+                if(c == '.'){
+                    playBeep(sourceDataLine, dotDuration);
+                    try {
+                        Thread.sleep(dotDuration);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else if(c == '-'){
+                    playBeep(sourceDataLine, dashDuration);
+                    try {
+                        Thread.sleep(dotDuration);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else if(c == '/'){
+                    playBeep(sourceDataLine, slashDuration);
+                    try {
+                        Thread.sleep(dotDuration);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                Thread.sleep(dotDuration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        sourceDataLine.drain();
+        sourceDataLine.stop();
+        sourceDataLine.close();
+    }
+
+    private void playBeep(SourceDataLine line, int duration){
+        byte[] data = new byte[duration * 44100 / 1000];
+        for (int i = 0; i < data.length; i++) {
+            // sin rate angle, based on the sample rate and frequency
+            double angle = i / (44100 / 440) * 2 * Math.PI;
+            data[i] = (byte) (Math.sin(angle) * 127);
+
+        }
+
+        line.write(data, 0, data.length);
+    }
+}
---
+++ MorseCode/src/MorseCodeGUI.java
@@ -0,0 +1,115 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
+public class MorseCodeGUI extends JFrame implements KeyListener{
+
+    private JTextArea inputTextArea, morseCodeTextArea;
+    private MorseBackend backend;
+
+    public MorseCodeGUI() {
+        super("Morse code translator");
+
+        setSize(new Dimension(540, 760));
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.decode("#3d3d29"));    // így lehet beállítani a JFrame hátterét
+        setLocationRelativeTo(null);
+
+        backend = new MorseBackend();
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        JLabel titleLabel = new JLabel("Morse code translator");
+        titleLabel.setFont(new Font("Old English Text MT", Font.PLAIN, 37));
+        titleLabel.setForeground(Color.white);
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setBounds(0,0,540, 100);
+
+        // text input
+        JLabel textinputLabel = new JLabel("Text: ");
+        textinputLabel.setFont(new Font("Old English Text MT", Font.PLAIN, 22));
+        textinputLabel.setForeground(Color.white);
+        textinputLabel.setBounds(20, 100, 200, 30);
+
+        inputTextArea = new JTextArea();
+        inputTextArea.setFont(new Font("Old English Text MT", Font.PLAIN, 22));
+        inputTextArea.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        inputTextArea.setLineWrap(true);
+        inputTextArea.setWrapStyleWord(true);
+        inputTextArea.addKeyListener(this);
+
+        JScrollPane scrollInput = new JScrollPane(inputTextArea);
+        scrollInput.setBounds(20, 132, 484, 200);
+
+        // morse output
+        JLabel morseLabel = new JLabel("Morse: ");
+        morseLabel.setFont(new Font("Old English Text MT", Font.PLAIN, 22));
+        morseLabel.setForeground(Color.white);
+        morseLabel.setBounds(20, 100, 200, 30);
+
+        morseCodeTextArea = new JTextArea();
+        morseCodeTextArea.setFont(new Font("Old English Text MT", Font.PLAIN, 22));
+        morseCodeTextArea.setEditable(false);
+        morseCodeTextArea.setLineWrap(true);
+        morseCodeTextArea.setWrapStyleWord(true);
+        morseCodeTextArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JScrollPane morseSCroll = new JScrollPane(morseCodeTextArea);
+        morseSCroll.setBounds(20, 400, 484, 200);
+
+        JButton playButton = new JButton("Play sound");
+        playButton.setBounds(210, 680, 150, 30);
+        playButton.setFont(new Font("Old English Text MT", Font.PLAIN, 22));
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                playButton.setEnabled(false);
+                Thread playMorseCodeThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] morseCodeMessage = morseCodeTextArea.getText().split(" ");
+                        backend.playSound(morseCodeMessage);
+                        playButton.setEnabled(true);
+                    }                    
+                });
+                playMorseCodeThread.start();
+            }
+        });
+
+        
+        add(titleLabel);
+        add(textinputLabel);
+        add(scrollInput);
+        add(morseSCroll);
+        add(playButton);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() != KeyEvent.VK_SHIFT){
+            String inputText = inputTextArea.getText();
+            morseCodeTextArea.setText(backend.translateToMorse(inputText));
+        }
+    }
+
+}
